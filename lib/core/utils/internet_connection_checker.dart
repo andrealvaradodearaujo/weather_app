5d05af431a419191f62cbb082daab33352f3e08e
Andre Alvarado <andrealvaradodearaujo@gmail.com>
2024-10-15 17:10:02 -0300

    updates pubspec; add fvm to gitignore; refactor InternetConnectionChecker

--- lib/core/utils/internet_connection_checker.dart
+++ lib/core/utils/internet_connection_checker.dart
@@ -9,29 +9,32 @@ class InternetConnectionChecker {
   ///
   /// Returns `true` if there is an internet connection, otherwise `false`.
   Future<bool> isInternetAvailable() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
+    var connectivityResults = await Connectivity().checkConnectivity();
 
-    if (connectivityResult == ConnectivityResult.mobile || connectivityResult == ConnectivityResult.wifi) {
+    // Verifica se a lista contém mobile ou wifi
+    if (connectivityResults.contains(ConnectivityResult.mobile) ||
+        connectivityResults.contains(ConnectivityResult.wifi)) {
       try {
         final result = await InternetAddress.lookup('example.com');
         if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
           try {
-            // Try making an HTTP request to a known server
-            var request = await HttpClient().headUrl(Uri.parse('https://www.google.com'));
+            var request =
+                await HttpClient().headUrl(Uri.parse('https://www.google.com'));
             var response = await request.close();
             var statusCode = response.statusCode;
-            return statusCode == HttpStatus.ok; // Indicates there is internet connectivity
+            return statusCode ==
+                HttpStatus.ok; // Verifica a conectividade da internet
           } catch (e) {
-            return false; // HTTP request error indicates lack of internet connectivity
+            return false; // Falha na requisição HTTP indica falta de conectividade
           }
         } else {
-          return false; // No valid response from DNS query
+          return false; // Falha na consulta DNS
         }
       } on SocketException catch (_) {
-        return false; // No internet connection
+        return false; // Sem conexão com a internet
       }
     } else {
-      return false; // No internet connection
+      return false; // Sem conexão de mobile ou wifi
     }
   }
 }
